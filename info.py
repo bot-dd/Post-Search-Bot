1f0a9426f624f0784ac493d872e871263e6c186d
bot-dd <botdd445@gmail.com>
2025-12-04 19:23:28 +0600

    Update API credentials and configuration values

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 26649585 
+API_HASH     = "588a3ea6fd01ae88bd2e10fed7d55b2c"
 BOT_TOKEN    = ""
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "codeflix_bots"
+DATABASE_URI = "mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg"
+LOG_CHANNEL  = -1002312610528
+ADMIN        = 7822720438
+CHANNEL      = "RmxBots"
